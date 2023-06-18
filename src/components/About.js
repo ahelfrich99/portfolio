526350a4e6cdda327288f898b5ad423d53fb6c0f
Adrianna Helfrich <adriannahelfrich99@gmail.com>
2023-06-17 20:27:41 -0700

    edited about

--- src/components/About.js
+++ src/components/About.js
@@ -4,10 +4,11 @@ export default function About() {
       <h2>About Me</h2>
       <h4>
         I'm Adrianna Helfrich! I'm a full-stack software engineer based in the
-        Bay Area. Though my background is in design, I recently completed Hack
-        Reactor's software engineering coding bootcamp and am searching for my
-        first job as a developer. Feel free to check out my projects on my
-        Gitlab, or take a look at my resume!
+        Bay Area. I recently completed Hack Reactor's Advanced Software
+        Engineering Immersive and and am now seeking my first job in the tech
+        industry. With a strong foundation in full-stack web development,
+        including experience with JavaScript, React, Node.js, and SQL, I'm
+        equipped to build innovative solutions and excited to learn even more.
       </h4>
     </div>
   );
