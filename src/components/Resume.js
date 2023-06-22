1e51ba75b9cd3505c4fae53d03d05e7ec7fb20f6
Adrianna Helfrich <adriannahelfrich99@gmail.com>
2023-06-21 17:47:59 -0700

    playing with resume css

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -7,17 +7,20 @@ import "../App.css";
 export default function Resume() {
   return (
     <div id="resume">
+      <br />
+      <a href={Adrianna_Helfrich_Resume} target={"_blank"} rel="noreferrer">
+        <Button className={"button"}>Download</Button>
+      </a>
+      <br />
       <img
         src={resume}
         alt="resume"
         style={{
           maxWidth: "70%",
+          margin: "3px",
         }}
       />
       <br />
-      <a href={Adrianna_Helfrich_Resume} target={"_blank"} rel="noreferrer">
-        <Button className={"button"}>Download</Button>
-      </a>
     </div>
   );
 }
