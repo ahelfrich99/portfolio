ff1e996d3cbaacd43007a87e85ff3bc72f360161
Adrianna Helfrich <adriannahelfrich99@gmail.com>
2023-06-23 10:27:20 -0700

    edited about

--- src/components/About.js
+++ src/components/About.js
@@ -6,9 +6,9 @@ export default function About() {
         I'm Adrianna Helfrich! I'm a full-stack software engineer based in the
         Bay Area. I recently completed Hack Reactor's Advanced Software
         Engineering Immersive and am now seeking my first job in the tech
-        industry. With experience building applications with JavaScript, React,
-        Django, FastAPI, and SQL, I'm equipped to build innovative solutions and
-        excited to learn even more.
+        industry. With experience building applications with React, Django,
+        FastAPI, and SQL, I'm equipped to build innovative solutions and excited
+        to learn even more.
       </h4>
     </div>
   );
