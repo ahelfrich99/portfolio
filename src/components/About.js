38898245ed1809f09ad4887cde4dd7a21aff1c42
Adrianna Helfrich <116464619+ahelfrich99@users.noreply.github.com>
2023-06-22 20:31:27 -0700

    Fixed type in About

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ export default function About() {
       <h4>
         I'm Adrianna Helfrich! I'm a full-stack software engineer based in the
         Bay Area. I recently completed Hack Reactor's Advanced Software
-        Engineering Immersive and and am now seeking my first job in the tech
+        Engineering Immersive and am now seeking my first job in the tech
         industry. With experience building applications with JavaScript, React,
         Django, FastAPI, and SQL, I'm equipped to build innovative solutions and
         excited to learn even more.
